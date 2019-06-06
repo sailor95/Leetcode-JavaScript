44f6d7a1a0fef215e16a410e57636fc41904b286
David Huang <david.huang@getac.com.tw>
2019-06-06 14:43:33 +0800

    [7] Reverse Integer

--- 7-reverse-integer/reverseInteger.js
+++ 7-reverse-integer/reverseInteger.js
@@ -1,7 +1,19 @@
 // v1: Pass, 80ms, 35.9MB
-var reverse = function (x) {
+var reverse_1 = function (x) {
   const ans = (parseFloat(x.toString().split('').reverse().join('')) * Math.sign(x))
   return (ans < (Math.pow(2, 31) - 1) && ans > -Math.pow(2, 31)) ? ans : 0;
 };
 
-// v2:
+// v2: Pass, 96ms, 35.6MB
+var reverse_2 = function (x) {
+  let result = 0;
+  let y = x * Math.sign(x);
+
+  while (y > 0) {
+    result = result * 10 + y % 10;
+    y = Math.floor(y / 10);
+  }
+  if (result > Math.pow(2, 31) - 1 || result < -Math.pow(2, 31)) return 0;
+  return result * Math.sign(x);
+}
+console.log(reverse_2(123));
