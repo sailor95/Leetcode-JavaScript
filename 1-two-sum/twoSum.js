602ed529b0cfed451f1de5543a9bf8597da071d7
David Huang <david.huang@getac.com.tw>
2019-06-10 15:14:25 +0800

    [1] Update

--- 1-two-sum/twoSum.js
+++ 1-two-sum/twoSum.js
@@ -34,4 +34,18 @@ var twoSum_3 = function (nums, target) {
   return [diffs.get(target - nums[j]), j];
 };
 
+// v4: 64ms
+var twoSum_4 = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+
 console.log(twoSum_1(nums, target));
