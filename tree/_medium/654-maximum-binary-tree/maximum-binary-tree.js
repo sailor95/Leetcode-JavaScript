d909234d5c8c11aa5f69bfa27a0b92c078c41f47
David Huang <david.huang@getac.com.tw>
2019-10-21 10:05:50 +0800

    [654][tree] Update ans

--- tree/_medium/654-maximum-binary-tree/maximum-binary-tree.js
+++ tree/_medium/654-maximum-binary-tree/maximum-binary-tree.js
@@ -35,6 +35,26 @@ var constructMaximumBinaryTree = function (nums) {
     return helper(nums, 0, nums.length - 1);
 };
 
+// 84 ms, faster than 93.62%
+const constructMaximumBinaryTree_1 = (nums, l = 0, r = nums.length - 1) => {
+    if (l > r) return null;
+
+    let maxIndex = l;
+
+    for (let i = l; i <= r; i++) {
+        if (nums[i] > nums[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    const node = new TreeNode(nums[maxIndex]);
+
+    node.left = constructMaximumBinaryTree(nums, l, maxIndex - 1);
+    node.right = constructMaximumBinaryTree(nums, maxIndex + 1, r);
+
+    return node;
+}
+
 const tester1 = [3, 2, 1, 6, 0, 5];
 
 console.log(constructMaximumBinaryTree(tester1));
