666cb10edc73a8beb03121b43da82d8def1a6c26
David Huang <bugman195@gmail.com>
2020-09-29 20:32:24 +0800

    [3][array] fizz buzz

--- array/easy/3-fizz-buzz/fizz-buzz.js
+++ array/easy/3-fizz-buzz/fizz-buzz.js
@@ -1,5 +1,23 @@
+const fizzBuzz1 = n => {
+  const result = [];
+
+  for (let i = 1; i <= n; i++) {
+    let result = '';
+    if (i % parseInt(3, 10) === 0) result += 'Fizz';
+    if (i % parseInt(5, 10) === 0) result += 'Buzz';
+
+    if (result.length) {
+      result.push(result);
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
 let fizzBuzz = function (n) {
-  let result = [];
+  const result = [];
 
   let mappings = {
     3: 'Fizz',
